b9474abe14545cf52313020b80d17aa1cb5ef720
Khang Le <khangsile@gmail.com>
2013-11-23 10:54:07 -0500

    initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '6.0'
+pod 'Reachability'
+pod 'JASidePanels', '~> 1.3.2'
+pod 'RestKit', '~> 0.20.0'
+
+# Testing and Search are optional components
+pod 'RestKit/Testing', '~> 0.20.0'
+pod 'RestKit/Search',  '~> 0.20.0'
---
+++ endthedebate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  endthedebate
+//
+//  Created by Khang Le on 11/23/13.
+//  Copyright (c) 2013 Khang Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
